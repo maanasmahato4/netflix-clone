8a43439bfcda4f02e3ab1206bbccc31fc4ab2e50
maanasmahato4 <maanasmahato456@gmail.com>
2024-03-28 03:07:31 +0545

    [update]: auth ui done

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,22 +1,76 @@
 'use client';
 import { Input } from '@/components/input';
-import { ChangeEvent, useState } from 'react';
+import { Button } from '@/components/ui/button';
+import { ChangeEvent, FormEvent, useState } from 'react';
 
 export default function Auth() {
 	const [username, setUserName] = useState<string>('');
+	const [email, setEmail] = useState<string>('');
+	const [password, setPassword] = useState<string>('');
+	const [formVariant, setVariant] = useState<string>('login');
+
+	async function handleSubmit(e: FormEvent): Promise<void> {
+		e.preventDefault();
+		console.log({ username, email, password });
+	}
+
+	function toggleFormVariant(): void {
+		setVariant((variant) => (variant === 'login' ? 'register' : 'login'));
+	}
+
 	return (
 		<section className='min-h-screen bg-[url("/images/hero.jpg")] bg-cover bg-fixed bg-no-repeat'>
 			<div className='flex min-h-screen items-center justify-center bg-black lg:bg-opacity-50'>
-				<form className='w-[20rem] rounded-lg bg-black p-8 opacity-80'>
+				<form
+					className='flex w-[25rem] flex-col gap-y-6 rounded-lg bg-black p-8 opacity-80'
+					onSubmit={handleSubmit}
+				>
+					<h3 className='text-xl'>
+						{formVariant === 'login' ? 'SignIn' : 'Register'}
+					</h3>
+					{formVariant === 'register' ? (
+						<Input
+							id='username'
+							label='Username'
+							onChange={(e: ChangeEvent<HTMLInputElement>) =>
+								setUserName(e.target.value)
+							}
+							value={username}
+							type={'text'}
+						/>
+					) : null}
 					<Input
-						id='gu'
-						label='name'
+						id='email'
+						label='Email'
 						onChange={(e: ChangeEvent<HTMLInputElement>) =>
-							setUserName(e.target.value)
+							setEmail(e.target.value)
 						}
-						value={username}
+						value={email}
 						type={'text'}
 					/>
+					<Input
+						id='password'
+						label='Password'
+						onChange={(e: ChangeEvent<HTMLInputElement>) =>
+							setPassword(e.target.value)
+						}
+						value={password}
+						type={'password'}
+					/>
+					<div>
+						<Button type='submit'>Submit</Button>
+					</div>
+					<p className='text-neutral-500'>
+						{formVariant === 'login'
+							? 'First time using NetFlix?'
+							: 'Already have an account?'}
+						<span
+							className='cursor-pointer text-white'
+							onClick={toggleFormVariant}
+						>
+							{formVariant === 'login' ? ' Create an account' : ' Login'}
+						</span>
+					</p>
 				</form>
 			</div>
 		</section>
