1ac5bf140fa62ea08da9560cec9d8f39ace3e81c
maanasmahato4 <maanasmahato456@gmail.com>
2024-03-29 00:18:38 +0545

    models done

---
+++ lib/database/mongoose.ts
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+declare global {
+	var mongoose: {
+		conn: any;
+		promise: any;
+	};
+}
+
+global.mongoose = {
+	conn: null,
+	promise: null,
+};
+
+export default async function dbConnect() {
+	if (global.mongoose.conn && global.mongoose.promise) {
+		console.log('database connection already established');
+		return global.mongoose.conn;
+	} else {
+		const mongodbUrl = process.env.MONGO_URL as string;
+		const connection = await mongoose.connect(mongodbUrl, { autoIndex: true });
+		global.mongoose = {
+			conn: connection,
+			promise: connection,
+		};
+		console.log('database connection established');
+		return connection;
+	}
+}
---
+++ lib/models/index.ts
@@ -0,0 +1,184 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema(
+	{
+		_id: {
+			type: mongoose.Schema.Types.ObjectId,
+			default: () => new mongoose.Types.ObjectId(),
+		},
+		name: {
+			type: String,
+			required: true,
+		},
+		image: {
+			type: String,
+			required: true,
+			default: '',
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		emailVerified: {
+			type: Date,
+		},
+		hashedPassword: {
+			type: String,
+			required: true,
+		},
+		sessions: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: 'Session',
+			},
+		],
+		accounts: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: 'Account',
+			},
+		],
+		favoriteIds: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+			},
+		],
+	},
+	{ timestamps: true },
+);
+
+const accountSchema = new mongoose.Schema(
+	{
+		_id: {
+			type: mongoose.Schema.Types.ObjectId,
+			default: () => new mongoose.Types.ObjectId(),
+		},
+		userId: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User',
+		},
+		type: {
+			type: String,
+			required: true,
+		},
+		provider: {
+			type: String,
+			required: true,
+		},
+		providerAccountId: {
+			type: String,
+			required: true,
+		},
+		refresh_token: {
+			type: String,
+		},
+		expires_at: {
+			type: Number,
+		},
+		token_type: {
+			type: String,
+		},
+		scope: {
+			type: String,
+		},
+		id_token: {
+			type: String,
+		},
+		session_state: {
+			type: String,
+		},
+	},
+	{ timestamps: true },
+);
+
+accountSchema.index({ provider: 1, providerAccountId: 1 }, { unique: true });
+
+const sessionSchema = new mongoose.Schema(
+	{
+		_id: {
+			type: mongoose.Schema.Types.ObjectId,
+			default: () => new mongoose.Types.ObjectId(),
+		},
+		sessionToken: {
+			type: String,
+			required: true,
+		},
+		userId: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User',
+			required: true,
+		},
+		expires: {
+			type: Date,
+		},
+	},
+	{ timestamps: true },
+);
+
+const verificationSchema = new mongoose.Schema(
+	{
+		_id: {
+			type: mongoose.Schema.Types.ObjectId,
+			default: () => new mongoose.Types.ObjectId(),
+		},
+		identifier: {
+			type: String,
+			required: true,
+		},
+		token: {
+			type: String,
+			unique: true,
+			required: true,
+		},
+		expires: {
+			type: Date,
+		},
+	},
+	{ timestamps: true },
+);
+
+verificationSchema.index({ identifier: 1, token: 1 }, { unique: true });
+
+const movieSchema = new mongoose.Schema(
+	{
+		_id: {
+			type: mongoose.Schema.Types.ObjectId,
+			default: () => new mongoose.Types.ObjectId(),
+		},
+		title: {
+			type: String,
+			required: true,
+		},
+		description: {
+			type: String,
+			required: true,
+		},
+		videoUrl: {
+			type: String,
+			required: true,
+		},
+		thumbnailUrl: {
+			type: String,
+			required: true,
+		},
+		genre: {
+			type: String,
+			required: true,
+		},
+		duration: {
+			type: String,
+			required: true,
+		},
+	},
+	{ timestamps: true },
+);
+
+export const UserModel = mongoose.model('User', UserSchema);
+export const AccountModel = mongoose.model('Account', accountSchema);
+export const SessionModel = mongoose.model('Session', sessionSchema);
+export const verificationModel = mongoose.model(
+	'Verification',
+	verificationSchema,
+);
+export const MovieModel = mongoose.model('Movie', movieSchema);
